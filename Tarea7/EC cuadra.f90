7c58c11d737b1a95d4eb3264dbd60fb27cfb5ea6
AbbyRamirez <abbyaimeermz@gmail.com>
2025-08-20 13:34:27 -0600

    Update and rename EC cuadra.f90 to Tarea7/EC cuadra.f90

--- Tarea7/EC cuadra.f90
+++ Tarea7/EC cuadra.f90
@@ -1,21 +1,21 @@
  PROGRAM EcuacionCuadratica
  Character(len = 1), PARAMETER:: imaginario = "i"
  REAL:: a,b,c,x1,x2,d,r
- WRITE(*,*)"Escribe los coeficientes a.b.c de la ecuaci¢n respectivamente"
+ WRITE(*,*)"Escribe los coeficientes a.b.c de la ecuaciÂ¢n respectivamente"
  READ(*,*)a,b,c
  d = (b**2) - (4*a*c)
  IF ( d == 0) THEN
-    WRITE(*,*)" Las ra¡ces son iguales y reales"
+    WRITE(*,*)" Las raÂ¡ces son iguales y reales"
     x1 = ((-1*b)+(d**0.5))/(2*a)
     x2 = ((-1*b)-(d**0.5))/(2*a)
      WRITE(*,*)"Las raices son: " ,x1,x2
  ELSE IF (d > 0) THEN
-    WRITE(*,*)" Las ra¡ces son diferentes y reales"
+    WRITE(*,*)" Las raÂ¡ces son diferentes y reales"
     x1 = ((-1*b)+(d**0.5))/(2*a)
     x2 = ((-1*b)-(d**0.5))/(2*a)
     WRITE(*,*)"Las raices son: " ,x1,x2
 ELSE
-    WRITE(*,*)" Las ra¡ces son conjugadas e imaginarias"
+    WRITE(*,*)" Las raÂ¡ces son conjugadas e imaginarias"
     r = (-1*b)/(2*a)
     x1 = ((-1*d)**0.5)/(2*a)
     WRITE(*,*)"Las raices son: ", r, "+" ,x1, "," ,r, "-" ,x2
