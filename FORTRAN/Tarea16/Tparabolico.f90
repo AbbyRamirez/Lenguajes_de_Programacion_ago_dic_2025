3808ff5d4aaebe1fbe55592133951b1a63758de2
AbbyRamirez <abbyaimeermz@gmail.com>
2025-09-04 20:59:32 -0600

    Update and rename Tparabolico.f90 to FORTRAN/Tarea16/Tparabolico.f90

--- FORTRAN/Tarea16/Tparabolico.f90
+++ FORTRAN/Tarea16/Tparabolico.f90
@@ -7,7 +7,7 @@ DO i = 1,90,1
   ymax = ((Vo**2)*(SIN(i*(3.1416/180))**2))/9.81
   vuelo = (19.62/ymax)**0.5
   xmax = (vo*COS(i*(3.1416/180)))*vuelo
-  WRITE(*,*)"angulo",i, "   Altura M xima ",ymax, "  Alcance M ximo",xmax
+  WRITE(*,*)"angulo",i, "   Altura MÂ xima ",ymax, "  Alcance MÂ ximo",xmax
   END DO
 PAUSE
 END PROGRAM Tparabolico
